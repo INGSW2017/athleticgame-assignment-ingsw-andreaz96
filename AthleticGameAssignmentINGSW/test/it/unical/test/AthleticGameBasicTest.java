9c1a70672572e8f9b268b92207ca36e5753ea47e
andreaz96 <andreaz96@hotmail.it>
2017-10-18 12:50:43 +0200

    Test

--- AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
+++ AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
@@ -1,5 +1,36 @@
 package it.unical.test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.time.Instant;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import it.unical.asde.AthleticGame;
+
 public class AthleticGameBasicTest {
 
+	private static AthleticGame game;
+
+	@BeforeClass
+	public static void initialize() {
+		game = new AthleticGame("staffetta");
+	}
+
+	// test per testare il metodo addArrival(String participant, Instant
+	// arrival). In questo test verificare che, aggiungendo un nuovo arrivo, la
+	// taglia della mappa arrivals diventi pari ad 1
+	@Test
+	public void addArrivalTest() {
+		game.addArrival("francesco", Instant.now());
+		assertEquals(1, game.getArrivals().size());
+	}
+
+	// test per verificare che getParecipiantTime(String participant) lanci
+	// un'eccezione in caso di partecipante non valido
+	@Test(expected=IllegalArgumentException.class)
+	public void getPartecipantTimeTest(){
+		game.getParecipiantTime("pinario");
+	}
 }
